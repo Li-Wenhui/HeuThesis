87e75b7c81537a742ab1c0e63a695ec2a53fc180
LIWenhui <lwh@vip.163.com>
2021-12-04 17:37:01 +0800

    修改示例文档。

--- build.bat
+++ build.bat
@@ -1,6 +1,1 @@
 make
-
-copy /y heuthesisbook.cls .\examples\book\master\.
-copy /y heuthesisbook.cfg .\examples\book\master\.
-copy /y heuthesis.sty .\examples\book\master\.
-copy /y heuthesis.bst .\examples\book\master\.
