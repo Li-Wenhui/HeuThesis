716a7eb3a5539a01321cd45687ccc39eebbdce37
LIWenhui <lwh@vip.163.com>
2022-03-27 11:53:39 +0800

    修改示例文档字体定义；修改批处理文件。

--- build.bat
+++ build.bat
@@ -1,1 +1,6 @@
-make
+xelatex heuthesis.ins
+xelatex heuthesis.dtx
+makeindex -s gind.ist -o heuthesis.ind heuthesis.idx
+makeindex -s gglo.ist -o heuthesis.gls heuthesis.glo
+xelatex heuthesis.dtx
+xelatex heuthesis.dtx
