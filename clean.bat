72c603402634316399ec280e55d8df2393ead326
LIWenhui <lwh@vip.163.com>
2026-03-01 13:54:36 +0800

    *：根据学校2026.2.28《关于启用新版《哈尔滨工程大学学位论文原创性声明和授权使用声明》的通知》（https://camel.hrbeu.edu.cn/2025/1231/c1233a345064/page.htm）修改原创性声明和授权使用声明。删除部分示例文件。

--- clean.bat
+++ clean.bat
@@ -1,1 +1,1 @@
-for %%a in (bak,log,aux,toc,dvi,thm,nav,snm,out,exa,bbl,blg,cut,idx,ilg,ind,lof,fen,toe,lot,ten,loa, run.xml) do (del *.%%a /f/s/q/a)
+for %%a in (bak,log,aux,toc,dvi,thm,nav,snm,out,exa,bbl,blg,cut,idx,ilg,ind,lof,fen,toe,lot,ten,loa, run.xml) do (del *.%%a /f/s/q/-h)
--- examples/book/chinese/build.bat
+++
@@ -1,8 +0,0 @@
-@echo Making xelatex......
-xelatex main.tex
-bibtex main
-xelatex main.tex
-xelatex main.tex
-
-del /S *.aux *.bbl *.blg *.out *.thm *.toc *.lof *.fen *.toe *.lot *.ten *.log *.bak *.loa *.glo *.idx *.ist
-exit
