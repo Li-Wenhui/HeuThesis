db680db66e5f79cb174b0b24bd2b0a86a400d78f
LIWenhui <lwh@vip.163.com>
2026-03-08 12:18:56 +0800

    *：修改批处理脚本。

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-xelatex heuthesis.ins
 xelatex heuthesis.dtx
+xelatex heuthesis.ins
 makeindex -s gind.ist -o heuthesis.ind heuthesis.idx
 makeindex -s gglo.ist -o heuthesis.gls heuthesis.glo
 xelatex heuthesis.dtx
